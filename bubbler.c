fbd5275467fa66af530a866ea6ebef3db83bd846
cecarroll <cecarrollwork@gmail.com>
2025-09-05 23:34:03 +0000

    ?z

--- bubbler.c
+++ bubbler.c
@@ -9,6 +9,12 @@ function sort (array):
                 printArray(array)
 
 
+
+Here, I am taking in 2 pointers to addresses of the values of array:
+    I can change each pointer to change the value at their addresses. If I change one pointer*, I change teh value at the address&.
+    If I change the pointer, it doesnt change the value at the &address. If I make arrayi = arrayj&, then I can still make arrayi& = arrayj
+
+
 function swap (array[i], array[j]):
 
 
@@ -27,7 +33,7 @@ const int MAX=9;
 
 void printValues(int*);
 //void sort(int*);
-//void swap(int*, int*);
+void swap(int*, int*);
 
 int main(){
   int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
@@ -38,13 +44,13 @@ int main(){
   int x = 3;
   int y = 5;
   printf("x: %d, y: %d \n", x, y);
-  //swap(&x, &y);
+  swap(&x, &y);
   printf("x: %d, y: %d \n", x, y);
 
   //sort(values);
   //printf("After: \n");
   printValues(values);
-
+  printf("\n");
   return(0);
 } // end main
 
@@ -59,3 +65,11 @@ void printValues(int *a){
     return;
 
 } 
+
+void swap(int *a, int *b){
+  int temp = *b;
+  *b  = *a;
+  *a = temp;
+
+
+}
