2a6c271e765bb9ecf998221c83cd86fd9b415747
cecarroll <cecarrollwork@gmail.com>
2025-09-06 03:27:40 +0000

    Final version Take 1

--- bubbler.c
+++ bubbler.c
@@ -1,8 +1,7 @@
 
-
+int keepGoing = 1;
 #include <stdio.h>
 const int MAX=9;
-int keepGoing = 1;
 
 void printValues(int*);
 void sort(int*);
@@ -12,14 +11,14 @@ int main(){
   int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
   printf("Before: \n");
   printValues(values);
-/*
+
   // test swap
   int x = 3;
   int y = 5;
   printf("x: %d, y: %d \n", x, y);
   swap(&x, &y);
   printf("x: %d, y: %d \n", x, y);
-*/
+
   sort(values);
   printf("After: \n");
   printValues(values);
@@ -28,15 +27,11 @@ int main(){
 } // end main
 
 void printValues(int *a){
-    
+    printf("["); 
     for (int i = 0; i < MAX ; i++) {
-        printf("%d", a[i]);
+        printf("%d ", a[i]);
     }
-
-    
-
-    return;
-
+    printf("]");
 } 
 
 void swap(int *a, int *b){
@@ -47,15 +42,13 @@ void swap(int *a, int *b){
 
 }
 
-void sort(int *a){
-  
+void sort(int *a){ 
   while(keepGoing){
     keepGoing = 0;
       for (int i = 0; i< MAX - 1;i++){
         if( *(a+i)>*(a+i+1)){
           swap((a+i),(a+i+1));
           keepGoing = 1;
-      	  printf("After: \n");
           printValues(a);
           printf("\n");
     
